18fb6bca87bc42ca5f2770dd67cb47c9589267ed
2026GLAI <2026glai@gmail.com>
2026-02-11 14:14:48 +0200

    fix: chat route /chat/:id

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
       </Routes>
     </Router>
   );
